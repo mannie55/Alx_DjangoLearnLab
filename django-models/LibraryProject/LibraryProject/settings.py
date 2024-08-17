d3ed91dfc6b1f4d1b5e794782709b10a70509c32
mannie55 <mannnie55@gmail.com>
2024-08-17 19:24:55 +0100

    reset everthing

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'relationship_app',
     'bookshelf.apps.BookshelfConfig',
     'django.contrib.admin',
     'django.contrib.auth',
