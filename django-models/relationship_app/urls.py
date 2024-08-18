69f7bc7c7f365757044eb42d7acba4684a67375e
mannie55 <mannnie55@gmail.com>
2024-08-18 13:20:33 +0100

    change the order of logout and login view in the url path

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,8 +5,8 @@ from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name = 'logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
