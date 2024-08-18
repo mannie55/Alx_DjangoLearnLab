7c80d94d9ee4816748cb45e24d40c41648a3f71a
mannie55 <mannnie55@gmail.com>
2024-08-18 16:22:51 +0100

    Implement Role-Based Access Control in Django

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,6 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('librarian', 'Librarian'),
+        ('member', 'Member'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_role_display()}"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+    
+
 
 class Author(models.Model):
     name = models.CharField(max_length=30)
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -24,15 +24,3 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
-
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             user = form.get_user()
-#             login(request, user)
-#             return redirect("list_books")
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, "login.html", {"form": form})
-
