a202d031cacd50ec49158eabd7ad8306333980fc
mannie55 <mannnie55@gmail.com>
2024-08-18 18:11:07 +0100

    fix

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import PermissionsMixin
 
 
 # Create your models here.
@@ -11,8 +12,9 @@ class UserProfile(models.Model):
         ('librarian', 'Librarian'),
         ('member', 'Member'),
     ]
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='member')
+    
 
     def __str__(self):
         return f"{self.user.username} - {self.get_role_display()}"
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,45 +1,36 @@
 from django.shortcuts import render, redirect
-from .models import Library, Book, UserProfile
-from django.contrib.auth import login
+from .models import Library 
+from .models import Book
 from django.views.generic.detail import DetailView
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib.auth.decorators import permission_required
 
 
-# Create your views here.
-# Function to check if user is an Admin
 def user_is_admin(user):
     return user.userprofile.role == 'admin'
 
-
-# Function to check if user is a Librarian
 def user_is_librarian(user):
     return user.userprofile.role == 'librarian'
 
-# Function to check if user is a Member
 def user_is_member(user):
     return user.userprofile.role == 'member'
 
-# Admin View
 @user_passes_test(user_is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    context = {'message': 'Welcome to the Admin Dashboard'}
+    return render(request, 'relationship_app/admin_view.html', context)
 
-
-
-# Librarian View
 @user_passes_test(user_is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
-
-# Member View
 @user_passes_test(user_is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
-
-
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
