663023d80b67060025cd183af707d6f6c3df4e13
mannie55 <mannnie55@gmail.com>
2024-08-18 19:11:20 +0100

    fix

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,36 +1,33 @@
 from django.shortcuts import render, redirect
-from .models import Library 
-from .models import Book
-from django.views.generic.detail import DetailView
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
+from .models import Library, Book, UserProfile
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test, login_required
-from django.contrib.auth.decorators import permission_required
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 
 
-def user_is_admin(user):
+def is_admin(user):
     return user.userprofile.role == 'admin'
 
-def user_is_librarian(user):
+def is_librarian(user):
     return user.userprofile.role == 'librarian'
 
-def user_is_member(user):
+def is_member(user):
     return user.userprofile.role == 'member'
 
-@user_passes_test(user_is_admin)
+@user_passes_test(is_admin)
 def admin_view(request):
-    context = {'message': 'Welcome to the Admin Dashboard'}
-    return render(request, 'relationship_app/admin_view.html', context)
+    return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(user_is_librarian)
+@user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
-@user_passes_test(user_is_member)
+@user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
