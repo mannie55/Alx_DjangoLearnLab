1cf8e92962c6396ef9de8a8f798e802c4a5d3fb2
mannie55 <mannnie55@gmail.com>
2024-08-18 13:18:28 +0100

    made some fix to imported views in urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
-from .views import register
+from . import views
 from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name = 'logout.html'), name='logout'),
     path('books/', list_books, name='list_books'),
