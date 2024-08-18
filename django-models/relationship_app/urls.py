5dd341d4b526dfa7ba0ceeb63436b58236b1fb34
mannie55 <mannnie55@gmail.com>
2024-08-18 17:38:34 +0100

    matched urls to permission views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
     path('register/', views.register, name='register'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,8 +3,45 @@ from .models import Library, Book
 from django.contrib.auth import login
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 
 # Create your views here.
+# Function to check if user is an Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+
+# Function to check if user is a Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+# Function to check if user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin View
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+
+# Librarian View
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+# Member View
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
 def list_books(request):
     books = Book.objects.all()
