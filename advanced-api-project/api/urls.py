021813776462e59f6cb294c94082709d720b59d2
mannie55 <mannnie55@gmail.com>
2024-09-08 10:55:35 +0100

    customized urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,10 +4,10 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('token/', views.obtain_auth_token),
-    path('book-list/', ListView.as_view(), name='book_list'),
-    path('book-detail/<int:pk>/', DetailView.as_view(), name='book-detail'),
-    path('book-create/', CreateView.as_view(), name='book-create'),
-    path('author-create-list/', CreateListAuthorView.as_view(), name='author-create'),
-    path('book-update/<int:pk>/', UpdateView.as_view(), name='book-update'),
-    path('book-delete/<int:pk>/', DeleteView.as_view(), name='book-delete'),
+    path('book/', ListView.as_view(), name='book_list'),
+    path('book/<int:pk>/', DetailView.as_view(), name='book-detail'),
+    path('book/', CreateView.as_view(), name='book-create'),
+    path('author/', CreateListAuthorView.as_view(), name='author-create'),
+    path('book/<int:pk>/', UpdateView.as_view(), name='book-update'),
+    path('book/<int:pk>/', DeleteView.as_view(), name='book-delete'),
 ]
