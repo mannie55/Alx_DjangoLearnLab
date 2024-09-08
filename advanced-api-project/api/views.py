902ee4b99f983b6e3590c0d86fb611234f18a1ad
mannie55 <mannnie55@gmail.com>
2024-09-08 11:05:38 +0100

    fix trailing spaces

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly,  IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
 
