a8012e202d94f27ebc6f4aa94cce3e802578335c
mannie55 <mannnie55@gmail.com>
2024-09-20 08:59:13 +0100

    create: model serializer

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
