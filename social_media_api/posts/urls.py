d1020e097c92c2ee33d9167a22fd49a9f8df5d27
mannie55 <mannnie55@gmail.com>
2024-09-22 19:44:16 +0100

    update

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -12,8 +12,8 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('<int:pk>/comment/', CommentOnPostView.as_view(), name='comment_on_post'),
-    path('like/<int:pk>/', LikePostView.as_view(), name='like_post'),
-    path('unlike/<int:pk>/', UnlikePostView.as_view(), name='unlike_post'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like_post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike_post'),
     path('feed/', PostFeedViewSet.as_view(), name='post_feed'),
     path('', include(router.urls)),
 ]
